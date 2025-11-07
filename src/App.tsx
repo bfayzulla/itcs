3766e8e85462c7d3893841ab662d7c82750d5c39
bfayzulla <b.fayzulla1987@gmail.com>
2025-11-07 16:03:56 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import { useState } from 'react'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-      Salom talabalar
-      </div>
-     
-    </>
+    <div>Hello</div>
   )
 }
 
