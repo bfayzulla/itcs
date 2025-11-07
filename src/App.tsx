ba850a20a59926ac3a73b10f6f3b05642d1e69e9
bfayzulla <b.fayzulla1987@gmail.com>
2025-11-07 16:02:58 +0500

    talabaSalom

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,9 @@ function App() {
   return (
     <>
       <div>
-      
+      Salom talabalar
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     
     </>
   )
 }
